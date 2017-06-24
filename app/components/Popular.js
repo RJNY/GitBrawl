734ce56881630f4fd8e11fba2fea58206d4d55b6
Richard Macias <rjny86@gmail.com>
2017-06-23 19:54:18 -0500

    Completed: Building a Highly Reusable Component

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types'
 import api from '../utils/api'
+import Loading from './Loading'
 
 function SelectLanguage(props) {
   var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
@@ -96,7 +97,7 @@ class Popular extends React.Component {
           onSelect={this.updateLanguage}
         />
         {!this.state.repos
-            ? <p> LOADING </p>
+            ? <Loading speed={200} />
             : <RepoGrid repos={this.state.repos} /> }
       </div>
     )
